0f2eccd49e20146e46e8d248f39241055897d7bd
Everton Kunz <ehokeverton@gmail.com>
2019-05-28 15:24:31 -0300

    First Commit.

---
+++ structs/structs-exercicio1.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+// Definição da constate responsável pelo tamanho do array
+#define TAM 2
+
+// Definição da struct
+struct cadastro {
+	char nome[30];
+	char mat[10];
+	int nota1, nota2;
+};
+
+int main() {
+	// Declaração do registro alunos usando a struct registro
+	struct cadastro alunos[TAM]; 
+	int i;
+	float media;
+	
+	// Laço responsável pela leitura dos valores
+	printf("\n---------- Cadastro de alunos -----------\n\n\n");
+	for (i=0;i<TAM;i++) {
+		printf("Cadastro numero %d\n", i+1);
+		printf("Insira nome..............: ");
+		scanf("%s", &alunos[i].nome);
+		printf("Insira a matricula.......: ");
+		scanf("%s", &alunos[i].mat);
+		printf("Insira a primeira nota...: ");
+		scanf("%d", &alunos[i].nota1);
+		printf("Insira a segunda nota....: ");
+		scanf("%d", &alunos[i].nota2);
+		printf("\n");
+	}
+	
+	// Laço responsável pela impressão dos valores
+	printf("\n---------- Medias dos alunos -----------\n\n\n");
+	for (i=0;i<TAM;i++) {
+		printf("Matricula.....: %s\n", alunos[i].mat);
+		printf("Nome..........: %s\n", alunos[i].nome);
+		media = (alunos[i].nota1 + alunos[i].nota2) /2;
+		printf("Media.........: %f\n", media);
+		printf("\n");
+	}
+	
+	return 0;
+}
