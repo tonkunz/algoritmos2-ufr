fcdb4f90c62baad779767ab76517843be7ce0d51
Fernando Santos <46528857+srfnd@users.noreply.github.com>
2019-10-25 10:52:40 -0400

    improvements

--- vetores/lista2/exercicio6.c
+++ vetores/lista2/exercicio6.c
@@ -1,43 +1,39 @@
 /*
  Desenvolva um algoritmo que leia um vetor de 10 elementos inteiros.
- Posteriormente, subtraia o valor da posição 0 pelo valor da próxima
- posição, e assim sucessivamente para todos os itens, exceto no último
- item/posição, onde você deve subtrair esse valor pelo valor da posição 0. Imprima o resultado.
+ Posteriormente, subtraia o valor da posiÃ§Ã£o 0 pelo valor da prÃ³xima
+ posiÃ§Ã£o, e assim sucessivamente para todos os itens, exceto no Ãºltimo
+ item/posiÃ§Ã£o, onde vocÃª deve subtrair esse valor pelo valor da posiÃ§Ã£o 0. Imprima o resultado.
 */
 
 #include <stdio.h>
 
-int main()
-{
+int main(){
+   // define o vetore e variaveis auxiliares 
+	  int i, sub, pos, vetor[10];
+  
+   // recebe os valores do vetor
+	  printf("Entre com valores para o vetor :\n\n");
+	  for (i=0; i<10; i++){
+		     printf("Insira o valor na pos[%d]:", i);
+		     scanf("%d", &vetor[i]);
+	   }
+ 
+    // Realizao a subtracao com o numero da posicao posterior do vetor
+	   for (i=0; i<10; i++){
+        
+        // caso ainda nao seja a ultima posicao, segue com a operacao normalmente
+		      if(i != 9){
+			     pos = i + 1;
+		     	sub = vetor[i] - vetor[i+1];
+	     		printf("\nsubtracao da pos[%d] pela pos[%d]: %d", i, pos, sub);
+	   	}
+     
+      // caso seja a ultima posicao, subtrai pelo valor da posicao 0
+	  	else {
+			     sub = vetor[9] - vetor[0];
+			     printf("\nsubtracao da pos[9] pela pos[0]: %d", sub);
+		  }
+	 }
+		
 
-    // Declarando o vetor e o vetor auxiliar
-    int vet[10], vet_aux[10], i;
-
-    // Lendo o vetor e o vetor auxiliar
-    for(i=0; i<10; i++)
-    {
-        printf("Insira o valor no vetor[%2d]: ",i);
-        scanf("%d", &vet[i]);
-        vet_aux[i]=vet[i];
-    }
-
-    printf("\n\n");
-
-    // Subtraindo as posições e imprimindo em sequência
-    for(i=0; i<10; i++)
-    {
-        if(i != 9)
-        {
-            vet[i] = vet_aux[i] - vet_aux[i+1];
-            printf("vetor[%2d]: %d\n",i,vet[i]);
-        }
-        else
-        {
-            vet[i] = vet_aux[i] - vet_aux[0];
-            printf("vetor[%2d]: %d\n",i,vet[i]);
-        }
-
-    }
-
-    return 0;
 }
