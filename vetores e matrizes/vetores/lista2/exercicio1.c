f04a520e65f819dde5c854c7280981a17180cfb9
Everton Kunz <ehokeverton@gmail.com>
2019-11-14 15:58:17 -0300

    Adiciona README para Vetores: Lista II
    Adicionado readme com o enunciado dos exercícios bem como ajustes nos
    comentários nos códigos das resoluções.

--- vetores e matrizes/vetores/lista2/exercicio1.c
+++ vetores e matrizes/vetores/lista2/exercicio1.c
@@ -1,6 +1,7 @@
 /*
- Desenvolva um algoritmo que leia um conjunto de 20 valores inteiros, guarde os em um vetor e escreva-os em ordem contrária.
- Por exemplo, trocar vetor[0] com vetor[19], trocar vetor[1] com vetor[18], etc;
+ Desenvolva um algoritmo que leia um conjunto de 20 valores inteiros, guarde os
+ em um vetor e escreva-os em ordem contrÃ¡ria. Por exemplo, trocar vetor[0] com
+ vetor[19], trocar vetor[1] com vetor[18], etc;
 */
 
 #include <stdio.h>
@@ -10,19 +11,18 @@ int main()
     // Declarando o vetor e o vetor auxiliar e o indice
     int vet[20], vet_aux[20], i, j;
 
-    // Laço de leitura do vetor
-    for(i=0; i<20; i++)
+    // LaÃ§o de leitura do vetor
+    for (i = 0; i < 20; i++)
     {
-        printf("Insira o valor no vetor[%2d]: ",i);
-        scanf("%d",&vet[i]);
+        printf("Insira o valor no vetor[%2d]: ", i);
+        scanf("%d", &vet[i]);
     }
 
-
-    // Laço para trocar as posições em ordem contrária com vetor auxiliar
-    j=19;
-    for(i=0; i<20; i++)
+    // LaÃ§o para trocar as posiÃ§Ãµes em ordem contrÃ¡ria com vetor auxiliar
+    j = 19;
+    for (i = 0; i < 20; i++)
     {
-        vet_aux[i]=vet[j];
+        vet_aux[i] = vet[j];
         j--;
     }
 
@@ -30,10 +30,10 @@ int main()
     printf("***Vetor em ordem contraria***");
     printf("\n\n");
 
-    // Laço de impressão
-    for(i=0; i<20; i++)
+    // LaÃ§o de impressÃ£o
+    for (i = 0; i < 20; i++)
     {
-        printf("Valor no vetor[%2d]:%2d\n",i, vet_aux[i]);
+        printf("Valor no vetor[%2d]:%2d\n", i, vet_aux[i]);
     }
 
     return 0;
--- vetores e matrizes/vetores/lista2/exercicio2.c
+++ vetores e matrizes/vetores/lista2/exercicio2.c
@@ -1,30 +1,29 @@
 /*
  Desenvolva um algoritmo que leia um conjunto de 10 valores reais, guarde-os em um vetor, calcule a soma dos
- valores armazenados em posições ímpares e escreva o resultado.
+ valores armazenados em posiÃ§Ãµes Ã­mpares e escreva o resultado.
 */
 #include <stdio.h>
 
 int main()
 {
-    // Declarando o vetor e a variável de soma
+    // Declarando o vetor e a variÃ¡vel de soma
 
     float vet[10], soma = 0;
     int i;
 
-    // Lendo e somando os valores nas posições ímpares na variável soma
-    for(i=0; i<10; i++)
+    // Lendo e somando os valores nas posiÃ§Ãµes Ã­mpares na variÃ¡vel soma
+    for (i = 0; i < 10; i++)
     {
-        printf("Insira o valor no vetor[%2d]: ",i);
-        scanf("%f",&vet[i]);
+        printf("Insira o valor no vetor[%2d]: ", i);
+        scanf("%f", &vet[i]);
 
-        // Verificar se a posição é impar, se for, somar na variável soma
-        if(i % 2 != 0)
-            soma+=vet[i];
+        // Verificar se a posiÃ§Ãµo Ã© Ã­mpar, se for, somar na variÃ¡vel soma
+        if (i % 2 != 0)
+            soma += vet[i];
     }
 
-    // Impressão do resultado
-    printf("\nResultado da soma das posicoes impares no vetor: %0.2f\n",soma);
-
+    // ImpressÃ£o do resultado
+    printf("\nResultado da soma das posicoes impares no vetor: %0.2f\n", soma);
 
     return 0;
 }
